2ba3edfc72b634513bcf1c937c700fc63e2e0c15
its-Pradyot <itkurkarpradyot@gmail.com>
2020-12-31 16:43:05 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-  import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  title = "Welcome to Angular";
+  arr = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+  loginFlag = false;
+  count = 1;
+
+  hello() {
+    this.title = "You clicked button";
+    console.log(" hello world");
+    if (this.loginFlag == false) {
+      this.loginFlag = true;
+    } else {
+      this.loginFlag = false;
+    }
+  }
+
+  bye() {
+    console.log("hi this is from text box");
+    this.name = "hi" + this.count;
+    this.count++;
+  }
 }
