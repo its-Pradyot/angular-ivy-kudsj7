8c3987d75468041ed82ccca11e511c25ed7ce61a
its-Pradyot <itkurkarpradyot@gmail.com>
2020-12-31 10:37:17 +0530

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { HelloComponent } from './hello.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ImagescrollerComponent } from './imagescroller/imagescroller.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AboutComponent, HomeComponent, ContactComponent ],
+  declarations: [ AppComponent, HelloComponent, AboutComponent, HomeComponent, ContactComponent, RegistrationComponent, LoginComponent, ImagescrollerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/imagescroller/imagescroller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-imagescroller',
+  templateUrl: './imagescroller.component.html',
+  styleUrls: ['./imagescroller.component.css']
+})
+export class ImagescrollerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
